13bba8d197e1eb4876cba19472334a8b81646a45
Franco Sarachu <fsarachu@gmail.com>
2016-11-27 18:25:35 -0300

    Add database_setup.py to catalog directory

---
+++ vagrant/catalog/database_setup.py
@@ -0,0 +1,34 @@
+# Configuration start
+import sys
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+# Classes and Mapping
+class Restaurant(Base):
+    __tablename__ = 'restaurant'
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+
+
+class MenuItem(Base):
+    __tablename__ = 'menu_item'
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
+
+
+# Configuration end
+engine = create_engine('sqlite:///restaurantmenu.db')
+
+Base.metadata.create_all(engine)
