00d36398d1999cc54a80aee9bbe3e5930dfa1cc9
Franco Sarachu <fsarachu@gmail.com>
2016-11-27 12:58:39 -0300

    Add create code

---
+++ vagrant/catalog/CRUD.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+# --- CREATE ---
+
+# New restaurant
+my_first_restaurant = Restaurant(name='Milanga House')  # Create new object
+session.add(my_first_restaurant)  # New object is now staged to be added to the database
+session.commit()  # Commit staged changes
+
+# New menu item
+napolitana = MenuItem(name='Napolitana', description='Tomato sauce, ham and cheese', course='Entree', price='$2.99',
+                      restaurant=my_first_restaurant)
+session.add(napolitana)
+session.commit()
