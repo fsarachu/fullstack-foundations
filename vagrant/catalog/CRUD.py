668c354b0bc4c2ee283940299086ed3e6ed2b32d
Franco Sarachu <fsarachu@gmail.com>
2016-11-27 13:08:41 -0300

    Move create code inside a function

--- vagrant/catalog/CRUD.py
+++ vagrant/catalog/CRUD.py
@@ -7,18 +7,20 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+
 # --- CREATE ---
 
-# New restaurant
-my_first_restaurant = Restaurant(name='Milanga House')  # Create new object
-session.add(my_first_restaurant)  # New object is now staged to be added to the database
-session.commit()  # Commit staged changes
+def create():
+    # New restaurant
+    my_first_restaurant = Restaurant(name='Milanga House')  # Create new object
+    session.add(my_first_restaurant)  # New object is now staged to be added to the database
+    session.commit()  # Commit staged changes
 
-# New menu item
-napolitana = MenuItem(name='Napolitana', description='Tomato sauce, ham and cheese', course='Entree', price='$2.99',
-                      restaurant=my_first_restaurant)
-chicken = MenuItem(name='Pollo', description='Just chicken', course='Entree', price='$1.99',
-                   restaurant=my_first_restaurant)
-session.add(napolitana)
-session.add(chicken)
-session.commit()
+    # New menu item
+    napolitana = MenuItem(name='Napolitana', description='Tomato sauce, ham and cheese', course='Entree', price='$2.99',
+                          restaurant=my_first_restaurant)
+    chicken = MenuItem(name='Pollo', description='Just chicken', course='Entree', price='$1.99',
+                       restaurant=my_first_restaurant)
+    session.add(napolitana)
+    session.add(chicken)
+    session.commit()
