16004d23ea86c6546188c5f33f9667826361a944
Franco Sarachu <fsarachu@gmail.com>
2016-11-27 18:42:53 -0300

    Add webserver.py

---
+++ vagrant/catalog/webserver.py
@@ -0,0 +1,44 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+
+class webserverHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            if self.path.endswith("/hello"):
+                self.send_response(200)
+                self.send_header('Content-Type', 'text/html')
+                self.end_headers()
+
+                output = """
+                    <!doctype html>
+                    <html lang='en'>
+                    <head>
+                        <meta charset='UTF-8'>
+                        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+                        <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+                        <title>Hola!</title>
+                    </head>
+                    <body>
+                      <h1>Hola!</h1>
+                    </body>
+                    </html>
+                """
+                self.wfile.write(output)
+
+        except IOError:
+            self.send_error(404, "File not found: {}".format(self.path))
+
+
+def main():
+    try:
+        port = 8080
+        server = HTTPServer(('', port), webserverHandler)
+        print "Web server running on port {}".format(port)
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print "^C entered, stopping server..."
+        server.socket.close()
+
+
+if __name__ == '__main__':
+    main()
