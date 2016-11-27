12bd315637504becb6e1e4f7b8a03b2c71d1d75b
Franco Sarachu <fsarachu@gmail.com>
2016-11-27 12:17:19 -0300

    Add sqlalchemy configuration

---
+++ vagrant/catalog/database_setup.py
@@ -0,0 +1,14 @@
+# Configuration start
+import sys
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+# Configuration end
+engine = create_engine('sqlite:///restaurantmenu.db')
+
+Base.metadata.create_all(engine)
