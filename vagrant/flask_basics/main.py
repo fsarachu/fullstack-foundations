6c8492af5568989c817b87c2c994ebfe8da8ffb1
Franco Sarachu <fsarachu@gmail.com>
2016-12-04 16:45:10 -0300

    Setup sqlalchemy

--- vagrant/flask_basics/main.py
+++ vagrant/flask_basics/main.py
@@ -1,4 +1,14 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Base
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 app = Flask(__name__)
 
