2d7fb3241f62179a8d67b715b44844741403d763
Franco Sarachu <fsarachu@gmail.com>
2016-12-05 13:22:58 -0300

    Add /restaurats route (list all restaurants feature)

--- vagrant/flask_basics/main.py
+++ vagrant/flask_basics/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -14,22 +15,22 @@ app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/hello')
-def hello_world():
-    restaurant = session.query(Restaurant).first()
-    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id).order_by(MenuItem.name.asc())
+@app.route('/restaurants/')
+def list_restaurants():
+    restaurants = session.query(Restaurant).all()
 
     output = ''
 
-    output += '<h1>{} menu:</h1>'.format(restaurant.name)
+    output += '<h1>Restaurants List</h1>'
     output += '<ul>'
 
-    for item in items:
-        output += '<li>{} - {} - {}</li>'.format(item.name, item.description, item.price)
+    for restaurant in restaurants:
+        output += '<li><a href="/restaurants/{}">{}</a></li>'.format(restaurant.id, restaurant.name)
 
     output += '</ul>'
 
     return output
+
 
 
 if __name__ == '__main__':
